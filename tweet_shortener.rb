20e051f371c7292f2c51ce43338e6189a374b2bb
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-24 15:56:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,30 +1,44 @@
 require 'pry'
 
 def dictionary
- { "hello" => "hi",
-["to, two, too"] => "2", 
-["for, four"] => "4",
-'be' => "b",
-'you' => "u",
-"at" => "@" ,
-"and" => "&"}
-
+  {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
 end
 
 def word_substituter(tweet)
-  new_tweet = []
   
-  new_tweet << tweet.split(" ")
-  
-  new_tweet.map do |word| 
-    if dictionary.keys.include?(word)
-      word = dictionary[word]
+  tweet.split(" ").map do |word| if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
     else
       word
     end
   end.join(" ")
+ end
  
- 
-
+def bulk_tweet_shortener(tweet)
+  tweet.each do |word| 
+  puts word_substituter(word)
+  
 end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    tweet.split(" ").map { |word| 
+  puts word_substituter(word) }
+else
+  puts tweet
+end
+end
+
+
 
